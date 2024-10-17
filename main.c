cfbbbaf46296ac690effc960350e4fac438247f8
MauricioRRojas <herramienta2250@gmail.com>
2024-10-17 16:51:28 +0000

    Cambios 17-10-24 2

--- main.c
+++ main.c
@@ -14,13 +14,38 @@ void appendNode(Node **head, const char *ruleIdentifier, const char *_production
 {
     Node *newNode = createNode(ruleIdentifier,production);
 
+
 }
 //Funcion para crear la lista desde el archivo
-Node *createLinkedlist()
+Node *createLinkedlist(FILE *file)
 {
     Node *head=NULL;
     char line[MAX_LINE_LENGTH];
-    
+    char ruleIdentifier[MAX_LINE_LENGTH];
+    char production[MAX_LINE_LENGTH];
+    //Leer el archivo linea por linea y guardar en un nuevo nodo
+    while(fgets(line, sizeof(line),file))
+    {
+        //remove the new line character if present;
+        line(strcspn(line,"w",)='\0');
+        //encontrar la posicion de "->" en la linea
+        const char *deletter = strstr(line,"->");
+        if(deletter != NULL)
+        {
+            //copear la parte después del simbolo "->"
+            strcpy(ruleIdentifier,line,deletter - line);
+            ruleIdentifier[delletter-line]='\0';
+            strcpy(production,deletter+2);
+            //Append the split line to the linked list
+            appendNode(&head,ruleIdentifier,production);
+        }
+        appendNode(&head,"",line);
+    }
+    return (head);
+}
+void printList(Node *head)
+{
+
 }
 int main()
 {
